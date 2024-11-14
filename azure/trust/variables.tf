53bfd21b478f2bbf1ce151849c279c27a7f2a041
Andrzej <andrzej.nowicki@ricoh-europe.com>
2024-11-14 13:09:59 +0000

    tidyup

--- azure/trust/variables.tf
+++ azure/trust/variables.tf
@@ -3,14 +3,14 @@
 
 variable "tfc_azure_audience" {
   type        = string
-  default     = "api://AzureADTokenExchange"
   description = "The audience value to use in run identity tokens"
+  default     = "api://AzureADTokenExchange"
 }
 
 variable "tfc_hostname" {
   type        = string
-  default     = "app.terraform.io"
   description = "The hostname of the TFC or TFE instance you'd like to use with Azure"
+  default     = "app.terraform.io"
 }
 
 variable "tfc_organization_name" {
@@ -20,12 +20,12 @@ variable "tfc_organization_name" {
 
 variable "tfc_project_name" {
   type        = string
-  default     = "Default Project"
   description = "The project under which a workspace will be created"
+  default     = "Default Project"
 }
 
 variable "tfc_workspace_name" {
   type        = string
-  default     = "my-azure-workspace"
   description = "The name of the workspace that you'd like to create and connect to Azure"
+  # default     = "my-azure-workspace"
 }
